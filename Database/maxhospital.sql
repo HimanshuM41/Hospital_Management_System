3c19122d42cb012f9800e79955fe58073fc38252
HimanshuM41 <133748195+HimanshuM41@users.noreply.github.com>
2023-05-17 00:18:30 +0530

    Add files via upload

---
+++ Database/maxhospital.sql
@@ -0,0 +1,287 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3307
+-- Generation Time: May 16, 2023 at 08:47 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `maxhospital`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `channel`
+--
+
+CREATE TABLE `channel` (
+  `channelno` varchar(255) NOT NULL,
+  `doctorname` varchar(255) NOT NULL,
+  `patientname` varchar(255) NOT NULL,
+  `roomno` int(11) NOT NULL,
+  `date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `channel`
+--
+
+INSERT INTO `channel` (`channelno`, `doctorname`, `patientname`, `roomno`, `date`) VALUES
+('CH001', 'DS001', 'PS003', 2, '2023-04-29'),
+('CH002', 'DS002', 'PS001', 1, '2023-04-21'),
+('CH003', 'DS001', 'PS002', 1, '2023-05-24');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `doctor`
+--
+
+CREATE TABLE `doctor` (
+  `doctorno` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `special` varchar(255) NOT NULL,
+  `qualification` varchar(255) NOT NULL,
+  `channelfee` int(11) NOT NULL,
+  `phone` bigint(11) NOT NULL,
+  `room` int(11) NOT NULL,
+  `log_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `doctor`
+--
+
+INSERT INTO `doctor` (`doctorno`, `name`, `special`, `qualification`, `channelfee`, `phone`, `room`, `log_id`) VALUES
+('DS001', 'Himanshu', 'Surgeon', 'MBBS', 15000, 9319321965, 1, 2),
+('DS002', 'Harry ', 'Cardiologist', 'MBBS', 20000, 9319327543, 2, 6);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `item`
+--
+
+CREATE TABLE `item` (
+  `itemid` varchar(255) NOT NULL,
+  `itemname` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `sellprice` int(11) NOT NULL,
+  `buyprice` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `item`
+--
+
+INSERT INTO `item` (`itemid`, `itemname`, `description`, `sellprice`, `buyprice`, `qty`) VALUES
+('IU001', 'Cofsils', 'cough n cold', 20, 10, 1000),
+('IU002', 'Dolo650', 'fever medicine', 40, 30, 500);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patient`
+--
+
+CREATE TABLE `patient` (
+  `patientno` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `phone` bigint(11) NOT NULL,
+  `address` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `patient`
+--
+
+INSERT INTO `patient` (`patientno`, `name`, `phone`, `address`) VALUES
+('PS001', 'ram', 9319321, 'India'),
+('PS002', 'Ravi', 6453629374, 'Ghaziabad UP India'),
+('PS003', 'Suresh', 9345637821, 'Uttarakhand');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `prescription`
+--
+
+CREATE TABLE `prescription` (
+  `pid` varchar(255) NOT NULL,
+  `channelid` varchar(255) NOT NULL,
+  `doctorname` varchar(255) NOT NULL,
+  `detype` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `prescription`
+--
+
+INSERT INTO `prescription` (`pid`, `channelid`, `doctorname`, `detype`, `description`) VALUES
+('PC001', 'CH001', 'Himanshu', 'Fever', 'Take dolo650 once a day'),
+('PC002', 'CH001', 'Himanshu', 'dengu', 'take dolo650 once a day');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales`
+--
+
+CREATE TABLE `sales` (
+  `id` int(11) NOT NULL,
+  `date` date NOT NULL,
+  `subtotal` int(11) NOT NULL,
+  `pay` int(11) NOT NULL,
+  `balance` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales`
+--
+
+INSERT INTO `sales` (`id`, `date`, `subtotal`, `pay`, `balance`) VALUES
+(1, '2023-04-29', 200, 300, 100),
+(2, '2023-04-29', 240, 500, 260),
+(3, '2023-04-29', 240, 500, 260);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sales_product`
+--
+
+CREATE TABLE `sales_product` (
+  `id` int(11) NOT NULL,
+  `sales_id` int(11) NOT NULL,
+  `prod_id` varchar(255) NOT NULL,
+  `sellprice` int(11) NOT NULL,
+  `qty` int(11) NOT NULL,
+  `total` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `sales_product`
+--
+
+INSERT INTO `sales_product` (`id`, `sales_id`, `prod_id`, `sellprice`, `qty`, `total`) VALUES
+(1, 3, 'IU001', 12, 20, 240);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `username` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `utype` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`, `username`, `password`, `utype`) VALUES
+(1, 'fghj', 'dfgh', '1234', 'Receptionist'),
+(2, 'Himanshu ', 'Himanshu41', 'harsh123', 'Doctor '),
+(3, 'bhavya', 'bhavya', '123', 'Receptionist'),
+(4, 'pranav', 'pranav1', '123', 'Pharmacist'),
+(5, 'aaron', 'aaron', '12345', 'Pharmacist'),
+(6, 'Harry', 'Harry', '1234', 'Doctor ');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `channel`
+--
+ALTER TABLE `channel`
+  ADD PRIMARY KEY (`channelno`);
+
+--
+-- Indexes for table `doctor`
+--
+ALTER TABLE `doctor`
+  ADD PRIMARY KEY (`doctorno`);
+
+--
+-- Indexes for table `item`
+--
+ALTER TABLE `item`
+  ADD PRIMARY KEY (`itemid`);
+
+--
+-- Indexes for table `patient`
+--
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientno`);
+
+--
+-- Indexes for table `prescription`
+--
+ALTER TABLE `prescription`
+  ADD PRIMARY KEY (`pid`);
+
+--
+-- Indexes for table `sales`
+--
+ALTER TABLE `sales`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `sales_product`
+--
+ALTER TABLE `sales_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `sales`
+--
+ALTER TABLE `sales`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `sales_product`
+--
+ALTER TABLE `sales_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
